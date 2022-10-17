2e459c07f15ce794c5724b7f440aa9083d58da29
GalijotasE <galijotase@gmail.com>
2022-10-17 10:18:01 +0300

    Click here to see changes

--- darbuotojai_programa.py
+++ darbuotojai_programa.py
@@ -15,7 +15,7 @@ class Menu:
         self.pavadinimas = Label(pagrindinis, text= "=== DARBUOTOJAI ===", width=17, font=("Bahnschrift", 25))
         self.darbuotoju_sarasas = Button(pagrindinis, text="Darbuotoju Sarasas", command=self.sarasas, width=25, font=("Bahnschrift", 20))
         self.naujas_darbuotojas = Button(pagrindinis, text="Naujas Darbuotojas", command=self.naujas, width=25, font=("Bahnschrift", 20))
-        self.darbuotojo_atnaujinimas = Button(pagrindinis, text="Atnaujinti Duomenis", width=25, font=("Bahnschrift", 20))
+        self.darbuotojo_atnaujinimas = Button(pagrindinis, text="Atnaujinti Duomenis", command=self.atnaujinimas, width=25, font=("Bahnschrift", 20))
         self.darbuotojo_istrynimas = Button(pagrindinis, text="Istrinti Darbuotoja",command=self.istrynimas, width=25, font=("Bahnschrift", 20))
         self.iseiti = Button(pagrindinis, text="Iseiti",command=self.isejimas, width=25, font=("Bahnschrift", 20))
         self.pavadinimas.pack(side=TOP)
@@ -30,7 +30,7 @@ class Menu:
         self.newWindow = tk.Toplevel(self.pagrindinis)
         self.frame = tk.Frame(self.pagrindinis)
         self.app = self.newWindow
-        self.newWindow.geometry("500x500")
+        self.newWindow.geometry("550x550")
         self.pavadinimas = Label(self.app, text="=== DARBUOTOJU SARASAS ===", font=("Bahnschrift", 25))
         self.sarasiukas = Listbox(self.app, height=400, width=450, selectmode=NONE)
         self.sarasiukas.insert(END, *database_sarasas())
@@ -42,7 +42,7 @@ class Menu:
         self.newWindow = tk.Toplevel(self.pagrindinis)
         self.frame = tk.Frame(self.pagrindinis)
         self.app = self.newWindow
-        self.newWindow.geometry("500x500")
+        self.newWindow.geometry("550x550")
         self.pavadinimas = Label(self.app, text="=== NAUJAS DARBUOTOJAS ===", font=("Bahnschrift", 25))
         self.u_vardas = Label(self.app, text="VARDAS",font=("Bahnschrift", 15))
         self.i_vardas = Entry(self.app)
@@ -71,7 +71,40 @@ class Menu:
         
 
     def atnaujinimas(self):
-        pass
+        self.newWindow = tk.Toplevel(self.pagrindinis)
+        self.frame = tk.Frame(self.pagrindinis)
+        self.app = self.newWindow
+        self.newWindow.geometry("550x550")
+        self.pavadinimas = Label(self.app, text="=== ATNAUJINTI DARBUOTOJA ===", font=("Bahnschrift", 25))
+        self.pasirinkite = Label(self.app, text="Pasirinkite darbuotoja", font=("Bahnschrift", 15))
+        self.sarasiukas = Listbox(self.app, width= 100, height= 7 ,selectmode=SINGLE)
+        self.sarasiukas.insert(END, *database_sarasas())
+        self.u_vardas = Label(self.app, text="VARDAS",font=("Bahnschrift", 15))
+        self.i_vardas = Entry(self.app)
+        self.u_pavarde = Label(self.app, text="PAVARDE",font=("Bahnschrift", 15))
+        self.i_pavarde = Entry(self.app)
+        self.u_gimimo_data = Label(self.app, text="GIMIMO DATA YYYY-MM-DD",font=("Bahnschrift", 15))
+        self.i_gimimo_data = Entry(self.app)
+        self.u_pareigos = Label(self.app, text="PAREIGOS",font=("Bahnschrift", 15))
+        self.i_pareigos = Entry(self.app)
+        self.u_atlyginimas = Label(self.app, text="ATLYGINIMAS",font=("Bahnschrift", 15))
+        self.i_atlyginimas = Entry(self.app)
+        self.prideti = Button(self.app, text="ATNAUJINTI", width=18, font=("Bahnschrift", 15))
+        self.pavadinimas.pack(side=TOP)
+        self.pasirinkite.pack()
+        self.sarasiukas.pack()
+        self.u_vardas.pack()
+        self.i_vardas.pack()
+        self.u_pavarde.pack()
+        self.i_pavarde.pack()
+        self.u_gimimo_data.pack()
+        self.i_gimimo_data.pack()
+        self.u_pareigos.pack()
+        self.i_pareigos.pack()
+        self.u_atlyginimas.pack()
+        self.i_atlyginimas.pack()
+        self.prideti.pack(side=BOTTOM)
+        self.frame.pack()
 
 
     def istrynimas(self):
@@ -101,7 +134,7 @@ class Menu:
 def pagrindinis_langas():
     langas = tk.Tk()
     app = Menu(langas)
-    langas.geometry("500x500")
+    langas.geometry("550x550")
     langas.title("DARBUOTOJAI - DUOMENU BAZE")
     langas.mainloop()
 
